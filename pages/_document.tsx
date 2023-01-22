eba90da70dcb9884f2305702e728e37e0c44a2f0
Arman Kazi <104687128+Amyx000@users.noreply.github.com>
2023-01-22 21:39:10 +0530

    meta updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,12 +8,12 @@ export default function Document() {
         <meta name="description" content="This is a Gif app that let you search all the gifs." />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:url" content="https://gif-app-lyart.vercel.app" />
         <meta property="og:title" content="Giphy Gifs" />
         <meta property="og:description" content="This is a Gif app that let you search all the gifs." />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:url" content="https://gif-app-lyart.vercel.app" />
         <meta property="twitter:title" content="Giphy Gifs" />
         <meta property="twitter:description" content="This is a Gif app that let you search all the gifs." />
       </Head>
